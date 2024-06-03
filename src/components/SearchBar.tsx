6a8e38f44805526547d0c8e0c989ed655b99e7af
Lion1446 <lionellediaz2557@gmail.com>
2024-06-04 01:05:04 +0800

    partial change test for 2 tab size

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,32 +1,32 @@
-import { InputAdornment, TextField } from "@mui/material";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import { ChangeEvent, ReactElement } from "react";
+import { InputAdornment, TextField } from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { ChangeEvent, ReactElement } from 'react';
 
 interface Props {
-	title: string;
-	value: string;
-	onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  title: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const SearchBar = ({ title, value, onChange }: Props): ReactElement => {
-	return (
-		<TextField
-			id='input-with-icon-textfield'
-			size='medium'
-			label={title}
-			style={{ flex: "1" }}
-			value={value}
-			onChange={onChange}
-			InputProps={{
-				startAdornment: (
-					<InputAdornment position='start'>
-						<SearchOutlinedIcon />
-					</InputAdornment>
-				),
-			}}
-			variant='outlined'
-		/>
-	);
+  return (
+    <TextField
+      id="input-with-icon-textfield"
+      size="medium"
+      label={title}
+      style={{ flex: '1' }}
+      value={value}
+      onChange={onChange}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchOutlinedIcon />
+          </InputAdornment>
+        )
+      }}
+      variant="outlined"
+    />
+  );
 };
 
 export default SearchBar;
